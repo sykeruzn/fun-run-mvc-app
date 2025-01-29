97bcf731b5c49da346ab83ccabd90783ade7b65b
Syruz Ken Domingo <141235021+sykeruzn@users.noreply.github.com>
2025-01-29 17:30:41 +0800

    feat: Add the frontend for the registration form under Home page

---
+++ FunRun2025/Models/Participants.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FunRun2025.Models
+{
+    public class Participants
+    {
+        [Required]
+        [Display(Name = "Given Name")]
+        public string GivenName { get; set; }
+
+        [Required]
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Required]
+        [Range(1, 120, ErrorMessage = "Please enter a valid age")]
+        public int Age { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        [Display(Name = "Shirt Size")]
+        public string ShirtSize { get; set; }
+    }
+}
